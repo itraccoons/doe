83fb8074ebe1c61465a6f81f41e2fa6125314ee9
raccoon1 <ok@raccoons.co>
2017-11-15 18:08:47 +0200

    adding commands to doe vagrant module

--- modules/doe.vagrant.sh
+++ modules/doe.vagrant.sh
@@ -1,12 +1,19 @@
 #/bin.sh
 
 case ${1:-help} in
-	machines) # XXX
-		find ~ -name .vagrant/machines
-		#vagrant global-status
+	meta) # Show all vagrant directories with metadata (.vagrant/)
+		find ~ -name .vagrant
 		;;
-	
-	help|*) # doe vagrant module help
+	clean) # Clean all vagrant metadata
+		find ~ -name .vagrant -exec rm -dr {} +
+		;;
+	latest) # Check vagrant updates
+		echo  "ver.installed:\t\c"
+		vagrant --version
+		echo "ver.latest:\t\c"
+		curl -s https://releases.hashicorp.com/vagrant/ | grep vagrant_ | head -1 | cut -d'>' -f2 | cut -d'<' -f1
+		;;
+	help|*) # Print doe vagrant module help
 		echo "Usage:\n\tdoe vagrant <subcommand>\n\nSubcommnads:"
 		grep ") #[ ]" $BASH_SOURCE | sed "s/) #/$(echo "\t")/g"
 		;;
